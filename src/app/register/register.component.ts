4cd112c8b23c2ce995e685538848377ae81bb907
Gabrielttbr <ttbr2888@gmail.com>
2021-07-23 16:47:58 -0300

    Validação do email feita

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,21 +8,38 @@ import { AppService } from '../app.service';
 })
 export class RegisterComponent implements OnInit {
   email: string = "";
-  senha: string ="";
+  senha: string = "";
+  EmailErro: string = "";
+  ConfirmSenha: string = "";
   constructor(private appService: AppService) { }
 
   ngOnInit(): void {
   }
-  emailKey(value:any){
+  emailKey(value: any) {
+    //Pegando  o valor do formulário
     this.email = value.target.value;
+
   }
-  senhaKey(value: any){
+  senhaKey(value: any) {
+    //Pegando o valor da senha
     this.senha = value.target.value;
-  } 
+  }
   login(){
-    const {email, senha} = this
-    this.appService.cadastro({email, senha})
+    //Fazendo o cadastro na minha api
+    const { email, senha } = this
+    this.appService.cadastro({ email, senha })
     console.log(this.email)
     console.log(this.senha)
+    //Validando email
+    if (this.email.length == 0) {
+      this.EmailErro = " [Error] Enter email! ";
+    } else if (this.email.indexOf('@') == -1) {
+      this.EmailErro = "[ ERROR ] We didn't find it @";
+    } else{
+
+    }
+  }
+  ConfirmeSenha(evento: any){
+    this.ConfirmSenha = evento.target.value;
   }
 }
